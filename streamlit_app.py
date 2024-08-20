f20fa6d5bdd898558e3eb5ac59c6719b6219d3ce
aledelat14 <0233516@up.edu.mx>
2024-08-20 17:10:06 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
-
-st.title("🎈 My new app")
+st.title("Alejandra de la Torre")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
