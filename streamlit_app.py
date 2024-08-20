214d4f5af842aba47e12231ca465ad8560a62090
aledelat14 <0233516@up.edu.mx>
2024-08-20 17:12:46 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,4 @@
 import streamlit as st
 st.title("Alejandra de la Torre")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+    "**Estudiante de Administración y Finanzas** en la Universidad Panamericana")
